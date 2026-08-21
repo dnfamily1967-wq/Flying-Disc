494faff2ff53c51cd81d09dbf6c0942f819d3997
dnfamily1967-wq <dnfamily1967@gmail.com>
2026-08-21 15:44:03 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "disc-team-maker-v2";
+const CACHE_NAME = "disc-team-maker-v3";
 const urlsToCache = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
